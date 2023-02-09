2f20b10d79b131a22e4e8873c0a1f9815877c984
a <a@as-MacBook-Air.local>
2023-02-09 14:48:36 +0530

    new file add

---
+++ index.js/loop.js
@@ -0,0 +1,103 @@
+// //1  1 se 100 tak print
+// for(let a=1;a<=100;a++){
+//     console.log(a);
+// }
+
+// //2  1 se 100 tak odd number
+// for(let b=1;b<=100;b++){
+//     if (b%3==0) {
+//         console.log(b,"odd");
+//     }
+// }
+
+// //3  1 se 100 tak even number
+// for(let c=0;c<=100;c++){
+//     if(c%2==0){
+//         console.log(c,"even");
+//     }
+// }
+
+// //4  all number multiple
+// for(let d=1;d<=100;d++){
+//     if(d%5==0){
+//         console.log(d);
+//     }
+// }
+
+// //5  multiple 5 or 15
+// for(let e=1;e<=100;e++){
+//     if (e%5==0&&e%15==0) {
+//         console.log(e);
+//     }
+// }
+
+// //6  2 ka table
+// for(let f=0;f<=20;f++){
+//     if(f%2==0){
+//         console.log(f);
+//     }
+// }
+
+// //7 hello w\print krwana hai prompt ke jriye se
+// let ans= Number(prompt("How many times do you want to print Hello World?" ));
+// for(let a=1;a<=ans;a++){
+//     console.log("hello");
+// }
+
+// //8  1 se 100 tak sum number
+// let sum = 0;
+// for(let z = 0; z <=100; z++){
+//     sum = sum + z
+// }
+// console.log(sum)
+
+// //9 even number 1se 100 tak sum no
+//  let sum =0
+// for(let i=1;i<=100;i++){
+//     if (i%2===0) {
+//         sum=sum+i
+//     }
+// }
+// console.log(sum,"even");  
+
+// //10 odd number 1 se 100 tak sum no
+// let sums=0
+// for(let x=1;x<=100;x++){
+//     if (x%2===1) {
+//         sums=sums+x
+//     }
+// }
+// console.log(sum,"odd");
+
+// //11
+// let a=1
+// while(a<=2048){
+//    console.log(a);
+//    a*=2
+//  }
+
+// //13
+// let b = 1
+// while (b <= 100) {
+//     console.log(b);
+//     b++
+// }
+
+// //14
+// let b = 0
+// while (b <= 100) {
+//     if (b % 3 == 0 && b % 5 == 0) {
+//         console.log("fizzbuzz");
+//     } else {
+//         console.log(b);
+//     } b++
+// }
+
+// //15
+// let a=1
+// while(a<=20){
+//     if (a%2===0) {
+//        console.log(a); 
+//     }
+//     a++
+// }
---
+++ index.js/paractice.js
@@ -0,0 +1,859 @@
+// alert("hello");
+// alert("Rustam");
+// alert("khan");
+// alert("deshwali");
+
+// var name="Rustam";
+// var mobile=9116795488;
+// var city="jaipur";
+// var coaching="wecode academy";
+// var village="ajmer";
+// var fullname="rustam khan";
+// var number=6377253061;
+// var friend="rafik";
+// var rustam="rustam";
+// var khan="khan";
+// console.log(name,mobile,city,coaching,village,fullname,number,friend,rustam,khan);
+
+// let Name="Rustam";
+// let Mobile=9116795488;
+// let City="jaipur";
+// let Coaching="wecode academy";
+// let Village="ajmer";
+// let Fullname="rustam khan";
+// let Number=6377253061;
+// let Friend="rafik";
+// let Rustam="rustam";
+// let Khan="khan";
+// console.log(Name,Mobile,City,Coaching,Village,Fullname,Number,Friend,Rustam,Khan);
+
+// const fathername="ali mohammmad";
+// const mothername="maina bano";
+// const foji="faruk";
+// const bhai="niyaj";
+// const masiaala="islam";
+// const bhuaaala="imran";
+// console.log(fathername,mothername,foji,bhai,masiaala,bhuaaala);
+
+// console.error("rustam");
+// console.error("khan");
+// console.error("deshwali");
+
+// console.warn("khan");
+// console.warn("rustam");
+// console.warn("deshwali");
+
+// let a=5;
+// let b=3;
+// let c=a+b
+// console.log(c);
+
+// let d=4;
+// let e=5;
+// console.log(d+e);
+
+// let f=5;
+// let g=3;
+// let h=f-g;
+// console.log(h);
+
+// let i=5;
+// let j=9;
+// console.log(i-j);
+
+// x = 5 
+// y = 10
+// console.log(x*y);
+
+
+// let r = 4
+// let s = 5
+// console.log(r,s);
+// console.log( "--------","++r,--s ,++r",++r,--s ,++r );   
+    
+// console.log( "--------2","r++,s--",r++,s--);
+
+// console.log( "--------3","--s",--s);
+
+// console.log( "--------4","r+-s",r+-s);
+
+// console.log( "--------5","r*s",r*s);
+
+//arthmetic opperator
+// +
+// -
+// *
+// /
+// **
+// %
+// ++
+// --
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(5**10);
+
+// compersion opreter 
+//  true false
+// =  value asign 
+// ==  value
+// ===  value and data type
+// <  less then
+// >   gerter then 
+// <=  choto he or braber 
+// >=   bdaa hao or braber 
+
+// console.log(10=="20");ye value same hogi to true agar value same nhi hogi to false 
+// ak string ho or ak no to bhi true
+// console.log(10===10);ye value or data type dono batata hai jaise value same hogi lekin ak value pr
+//  double code bhi hai to vo false hai agr dono valu par double code ho to true */
+//  console.log(20<10); 20 bdi hai 10 
+//  console.log(20<=20); 20 brabar hai 20 
+//  console.log(20<30); 20 chota hai 30
+//  console.log(20>10);20 badi hai 10
+//  console.log(10>20); 10 chota hai 20
+//  console.log(30>=20);30 barabar nhi hai 20
+
+//  console.log(30<10);
+//  console.log(40<50);
+//  console.log(50<=40);
+//  console.log(20==30);
+//  console.log(40===40);
+
+//  console.log(20==30);
+//  console.log(20===10);
+//  console.log(20>10);
+//  console.log(20<10);
+//  console.log(20<=40);
+//  console.log(20>=70);
+
+// var x=117
+// if (x%2==0 && x%4==0) {
+//     console.log("sam");
+// }else{
+//     console.log("visam");
+// }
+
+//1
+// var a=122
+// if (a>60) {
+//     console.log("pass");
+// }else{
+//     console.log("fail");
+// }
+
+// //2
+// let b=11
+// let c=61
+// if (b%2==1 && c%2==1) {
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// //3
+// let d=70
+// let e=20
+// let f=60
+// let g=20
+// console.log(d=e);
+// console.log(e=f);
+// console.log(f=g);
+// console.log(g=d);
+
+// //5
+// console.log(2+5*10-20**2);
+// console.log(20/5*10+10);
+// console.log(10 && 20 + 10 * 10);
+// console.log(30 > 20 && 10 + 4 < 15);
+// console.log(30 > 20 && 10 + 4 < 15 || 10);
+// console.log(30 < 20 || 10 + 5 < 15 || 10);
+// console.log(30 < 20 || 10 + 5 < 15 && 10);
+
+// //6
+// let h=55
+// if (h%2==0) {
+//     console.log("yes");
+// }else if (h%2==1) {
+//     console.log("no");
+// }
+
+// //7
+// let i=125
+// if (i%10==0 && i%5==0) {
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// //8
+// let j=5
+// if (j*20==100) {
+//    console.log("yes"); 
+// }else{
+//     console.log("no");
+// }
+
+// //9
+// var k=64
+// if (k>70) {
+//     console.log("fail");
+// }else{
+//     console.log("pass");
+// }
+
+// //10
+// let l=4
+// if (l>21) {
+//     console.log("adult");
+// } else{
+//     console.log("minar");
+// }
+
+// //1
+// (l>21) ? console.log("adult"):console.log("minar");
+
+// //2
+// let percentage=54
+// if (percentage>=80) {
+//     console.log("mariat");
+// }else if (percentage>=60&&percentage<80) {
+//     console.log("first");
+// }else if (percentage>=33&&percentage<60) {
+//     console.log("second");
+// }else if (percentage<33) {
+//     console.log("third");
+// }
+
+// //3
+// let m=56
+// let n=89
+// if(m>n){
+//     console.log(m);
+// }else if(n>m){
+//  console.log(n);
+// }else{
+//     console.log("both number are sm");
+// }
+
+// //4
+// let o=30
+// let p=58
+// let q=98
+// if (o>p>q) {
+//     console.log(o);
+// }else if (q>p>o) {
+//     console.log(p);
+// }else if (q>p<o) {
+//     console.log(q);
+// }else{
+//     console.log("both number are sm");
+// }
+
+//year
+// let year = 2023
+// let rafik = 2000
+// let jabbar = 2002
+// let ajeet = 2001
+// let r = year-rafik
+// let ja = year-jabbar
+// let a = year-ajeet
+// if (r>=ja&&r>=a) {
+//     console.log("rafik")
+// }else if(ja>=a&&ja>=r){
+//     console.log("jabbar");
+// }else {
+//     console.log("ajeet");
+// }
+    
+//year
+// let year = 2023
+// let devendar = 2004
+// let bittu = 2002
+// let soyab = 2000
+// let d = year-devendar
+// let b = year-bittu
+// let s = year-soyab
+// if (d>=b&&d>=s) {
+//     console.log("devendar");
+// }else if (b>=s&&b>=d) {
+//     console.log("bittu");
+// }else{
+//     console.log("soyab");
+// }
+
+//1
+// let a=8
+// let b=9
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// //2
+// let x=262
+// if (x%2==0) {
+//     console.log("sam");
+// }else{
+//     console.log("visam");
+// }
+
+//3
+// let ab=(prompt("my name is rustam"))
+// console.log(ab);
+
+//4
+// let a=6
+// let b=12
+// if (a<b) {
+//    console.log("rustam");
+//    console.log(18);
+//    console.log(9116795488);
+// } else {
+//     console.log("no");
+// }
+
+//5
+// let z=69
+// if (z%5==0&&z%10==0) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+//6
+// var q="hello"
+// alert(q)
+// console.log(q);
+
+//7
+// let user=null
+// let a=0
+// let b=undefined
+// let c=NaN
+// let d=false
+
+// if (user) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if (a) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if (b) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if (c) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if (d) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+//10
+// let a=0
+// let b=10
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(++a);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+// console.log(--b);
+
+//9
+// var t=3
+// var u=5
+// if (t>5) {
+//     console.log("big family");
+// }else{
+//     console.log("small family");
+// }
+
+//8
+// let w=confirm("Do you want to play")
+// if (w) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// } 
+
+// for (let a = 0 ; a <= 20 ; a++) {
+//     if (a % 2 == 0) {
+//         console.log(a);
+//     }
+    
+// }
+
+// for (let k = 0;k<=10;k++) {
+// console.log(k);
+// console.log("hello");
+// }
+
+// for (let a = 0; a <= 60; a++){
+//     if (a%6==0) {
+//         console.log(a);
+//     }
+// }
+
+// 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
+
+// for(let i=1;i<=100;i++){
+//     console.log(i,"odd");
+//     i++
+// }
+
+// for(let b=2;b<=100;b++){
+//     console.log(b, "even");
+//     b++
+//}
+
+// for(let a=0;a<=100;a++){
+//     if (a%5==0) {
+//        console.log(a); 
+//     }
+// }
+
+// for(let b=0;b<=100;b++){
+//     if (b%5==0&&b%15==0) {
+//        console.log(b); 
+//     }
+// }
+
+////1
+// for(let a=1;a<=100;a++){
+//     console.log(a);
+// }
+
+// //2
+// for(let b=1;b<=100;b++){
+//     if (b%3==0) {
+//         console.log(b,"odd");
+//     }
+// }
+
+// //3
+// for(let c=0;c<=100;c++){
+//     if(c%2==0){
+//         console.log(c,"even");
+//     }
+// }
+
+// //4
+// for(let d=1;d<=100;d++){
+//     if(d%5==0){
+//         console.log(d);
+//     }
+// }
+
+////5
+// for(let e=1;e<=100;e++){
+//     if (e%5==0&&e%15==0) {
+//         console.log(e);
+//     }
+// }
+
+// //6
+// for(let f=0;f<=20;f++){
+//     if(f%2==0){
+//         console.log(f);
+//     }
+// }
+
+// //7
+// let ans= Number(prompt("How many times do you want to print Hello World?" ));
+// for(let a=1;a<=ans;a++){
+//     console.log("hello");
+// }
+
+// //8
+// let sum = 0;
+// for(let z = 0; z <=100; z++){
+//     sum = sum + z
+// }
+// console.log(sum)
+
+//9
+// for(let x=1;x<=100;x++){
+//     if (x%2==0) {
+//         console.log(x,"even");
+//     }else{
+//         console.log(x,"odd");
+//     }
+// }
+
+// //star print
+// let x,y
+// for(let x=1;x<=5;x++){
+//     for(let y=1;y<=x;y++){
+//         console.log("*");
+//     }
+//     console.log("\n");
+// }
+// return(0);
+
+// let sum=0
+// for(let a=1;a<=50;a++)
+//     if (a%7===0) {
+//         sum=sum+a
+//     }
+//     console.log(sum);
+
+// let a=1
+// while(a<=100){
+//     console.log(a);
+//     a++
+// }
+
+//let b=1
+// while(b<=100){
+//     if (b%2===1) {
+//         console.log(b);
+//     }
+//     b++
+// }
+
+// //13
+// let b=1
+// while (b<=100) {
+//     console.log(b);
+//     b++
+// }
+
+// //15
+// let a=1
+// while(a<=100){
+//     if (a%10===0) {
+//        console.log(a); 
+//     }
+//     a++
+// }
+
+// //11
+// let a=1
+// while(a<=2048){
+//    console.log(a);
+//    a*=2
+//  }
+
+// //14
+// let b = 0
+// while (b <= 100) {
+//     if (b % 3 == 0 && b % 5 == 0) {
+//         console.log("fizzbuzz");
+//     } else {
+//         console.log(b);
+//     } b++
+// }
+
+//12
+
+// console.log('"hello"');
+
+// let a="rustam khan deshwali"
+// console.log(a.replace("khan","mohammad"));
+
+// for(let a=1;a<=2048;a*=2){
+//     console.log(a);
+// }
+
+// //confirm
+// let a=confirm("Rustam khan jaipur")
+// if (a) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// // percentage
+// var per=prompt("your percentage")
+// if (per>=80&&per<=100) {
+//     console.log("merit");
+// } else if (per>=60&&per<=80) {
+//     console.log("first division");
+// }else if (per>=45&&per<=60) {
+//     console.log("second division");
+// } else if (per>=33&&per<=45) {
+//     console.log("third division");
+// }else if(per<=33){
+//     console.log("Fail");
+// }else{
+//     console.log("valid percentage");
+// }
+
+// //function
+// function a(){
+//     console.log("rustam");
+// }
+// function b() {
+//     console.log("khan");
+// }
+// function c() {
+//     console.log("jaipur");
+// }
+
+// a();
+// b();
+// c();
+
+//do while loop
+// let a=1
+// do {
+//     if (a%2==0) {
+//       console.log(a);  
+//     }
+// } while (a<=10);
+
+//for loop
+// //1
+// for(let a=1;a<=100;a++){
+//     console.log(a);
+// }
+
+// //2
+//  for(let b=1;b<=100;b++){
+//     if (b%2==1) {
+//         console.log(b,"odd");
+//     }
+//  }
+
+//  //3
+//  for(let c=1;c<=100;c++){
+//     if (c%2==0) {
+//         console.log(c,"even");
+//     }
+//  }
+
+//  //4
+//  for(let d=1;d<=100;d++){
+//     if (d%5==0) {
+//         console.log(d);
+//     }
+//  }
+
+//  //5
+//  for(let e=1;e<=100;e++){
+//     if (e%5==0&&e%15==0) {
+//         console.log(e);
+//     }
+//  }
+
+// //6
+// for(let f=1;f<=20;f++){
+//     if (f%2==0) {
+//         console.log(f);
+//     }
+// }
+
+// //7
+// for(let g=1;g<=5;g++){
+//     if (g%1==0) {
+//         console.log(g,"hello");
+//     }
+// }
+
+// //8
+// let sum = 0;
+// for(let h = 0; h <=100; h++){
+//     sum = sum + h
+// }
+// console.log(sum)
+
+// //9
+// let sums=0
+// for(let i=1;i<=100;i++){
+//     if (i%2===1) {
+//         sums=sums+i
+//     }
+// }
+// console.log(sums,"odd");
+
+// //10
+// let som=0
+// for(let j=1;j<=100;j++){
+//     if (j%2==0) {
+//         som =som+j
+//     }
+// }
+// console.log(som,"even");
+
+// //11
+// for(let k=1;k<=2048;k*=2){
+//     if (k%2==0) {
+//         console.log(k);
+//     }
+// }
+
+// // //12
+// for(let s=1;s<=100;s=s+10){
+//      console.log(s);
+//       for(let t=s;t<s+10;t++){
+//             document.write(t)
+//       }
+//       document.write("<br>")
+// }
+
+// //13
+
+// for (let l = 0;l <= 100;l++) {
+//     if (l % 3 == 1 && l % 5 == 1) {
+//         console.log("fizzbuzz");
+//     } else {
+//         console.log(l);
+//     } 
+// }
+
+// //14
+// for(let r=1;r<=10;r++){
+//     console.log(2+"*"+r+"="+r*2);
+// }
+
+//15
+// for(let y=1;y<=50;y=y+10){
+//      for(let z=y;z<y+10;z++){
+//           document.write(z+",")
+//      }
+//      document.write("<br>")
+// }
+
+// //16
+// for(let ab=50;ab>=1;ab=ab-10){
+//      for(let bc=ab;bc>ab-10;bc--){
+//           document.write(bc+",")
+//      }
+//      document.write("<br>")
+// }
+
+// //17
+// for(let o=1;o<=10000;o*=5){
+//     if (o%5==0) {
+//         console.log(o);
+//     }
+// }
+
+// //18
+// for(let n=1;n<=500;n++){
+//     if (n%3==0) {
+//         console.log(n);
+//     }
+// }
+
+// //19
+// for (let m=120;m<=1029;m++){
+//     if (m%2==0) {
+//         console.log(m);
+//     }
+// }
+
+// //20
+// for(let p=100;p>=0;p--){
+//     if (p%5===0) {
+//         console.log(p);
+//     }
+// }
+
+// //while loop
+// //1
+// let q=1
+// while(q<=10){
+//     console.log(q);
+//     q++
+// }
+
+//2
+// let u=1
+// while(u<=10){
+//      if (u%2==0) {
+//           console.log(u);
+//      }
+//      u++
+// }
+
+// //3
+// let v=10
+// while (v>=1) {
+//      console.log(v);
+//      v--
+// }
+
+// //4
+// let w=1
+// while (w<=20) {
+//      if (w%2==0) {
+//           console.log(w);
+//      }
+//      w++
+// }
+
+//  //5
+// let sums=0
+// let x=1
+// while(x<=100){
+//     if (x%2===1) {
+//         sums=sums+x
+//     }
+//     x++
+// }
+// console.log(sums,"odd");
+
+//do while loop
+// //1
+// let cd=1
+// do {
+//     console.log(cd);
+//     cd++
+// } while (cd<=5);
+
+// //2
+// let de=10
+// do {
+//     console.log(de);
+//     de--
+// } while (de>=1);
+
+// //3
+// let sum=0
+// let ef=1
+// do {
+//     sum=sum+ef
+//     ef++
+// } while (ef<=10);
+// console.log(sum);
+
+// //4
+// let fg=1
+// do {
+//     if (fg%2==0) {
+//         console.log(fg);
+//     }
+//    fg++
+// } while (fg<=20);
