3b3b78353c32651f7b5f3fe644663aa92a849aa4
a <a@as-MacBook-Air.local>
2023-02-15 18:33:38 +0530

    first

---
+++ index.js/break&continue.js
@@ -0,0 +1,22 @@
+// //1
+// for(let a=1;a<=10;a++){
+//     if (a==3) {
+//         continue;
+//     }
+//     if (a==9) {
+//        break 
+//     }
+//     console.log(a);
+// }
+
+// //2
+// for (let a = 1; a <= 10; a++) {
+//     if (a >= 5 && a <= 8) {
+//         continue;
+//     }
+//     if (a == 8) {
+//         break;
+//     }
+//     console.log(a);
+
+// }
--- index.js/loop.js
+++ index.js/loop.js
@@ -1,80 +1,80 @@
 // //1  1 se 100 tak print
-// for(let a=1;a<=100;a++){
+// for (let a = 1; a <= 100; a++) {
 //     console.log(a);
 // }
 
 // //2  1 se 100 tak odd number
-// for(let b=1;b<=100;b++){
-//     if (b%3==0) {
-//         console.log(b,"odd");
+// for (let b = 1; b <= 100; b++) {
+//     if (b % 3 == 0) {
+//         console.log(b, "odd");
 //     }
 // }
 
 // //3  1 se 100 tak even number
-// for(let c=0;c<=100;c++){
-//     if(c%2==0){
-//         console.log(c,"even");
+// for (let c = 0; c <= 100; c++) {
+//     if (c % 2 == 0) {
+//         console.log(c, "even");
 //     }
 // }
 
 // //4  all number multiple
-// for(let d=1;d<=100;d++){
-//     if(d%5==0){
+// for (let d = 1; d <= 100; d++) {
+//     if (d % 5 == 0) {
 //         console.log(d);
 //     }
 // }
 
 // //5  multiple 5 or 15
-// for(let e=1;e<=100;e++){
-//     if (e%5==0&&e%15==0) {
+// for (let e = 1; e <= 100; e++) {
+//     if (e % 5 == 0 && e % 15 == 0) {
 //         console.log(e);
 //     }
 // }
 
 // //6  2 ka table
-// for(let f=0;f<=20;f++){
-//     if(f%2==0){
+// for (let f = 0; f <= 20; f++) {
+//     if (f % 2 == 0) {
 //         console.log(f);
 //     }
 // }
 
 // //7 hello w\print krwana hai prompt ke jriye se
-// let ans= Number(prompt("How many times do you want to print Hello World?" ));
-// for(let a=1;a<=ans;a++){
+// let ans = Number(prompt("How many times do you want to print Hello World?"));
+// for (let a = 1; a <= ans; a++) {
 //     console.log("hello");
 // }
 
 // //8  1 se 100 tak sum number
 // let sum = 0;
-// for(let z = 0; z <=100; z++){
+// for (let z = 0; z <= 100; z++) {
 //     sum = sum + z
 // }
 // console.log(sum)
 
 // //9 even number 1se 100 tak sum no
-//  let sum =0
-// for(let i=1;i<=100;i++){
-//     if (i%2===0) {
-//         sum=sum+i
+// let sum = 0
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         sum = sum + i
 //     }
 // }
-// console.log(sum,"even");  
+// console.log(sum, "even");
 
 // //10 odd number 1 se 100 tak sum no
-// let sums=0
-// for(let x=1;x<=100;x++){
-//     if (x%2===1) {
-//         sums=sums+x
+// let sums = 0
+// for (let x = 1; x <= 100; x++) {
+//     if (x % 2 === 1) {
+//         sums = sums + x
 //     }
 // }
-// console.log(sum,"odd");
+// console.log(sum, "odd");
 
 // //11
-// let a=1
-// while(a<=2048){
-//    console.log(a);
-//    a*=2
-//  }
+// let a = 1
+// while (a <= 2048) {
+//     console.log(a);
+//     a *= 2
+// }
 
 // //13
 // let b = 1
@@ -94,10 +94,313 @@
 // }
 
 // //15
-// let a=1
-// while(a<=20){
-//     if (a%2===0) {
-//        console.log(a); 
+// let a = 1
+// while (a <= 20) {
+//     if (a % 2 === 0) {
+//         console.log(a);
 //     }
 //     a++
+// }
+
+// for loop
+// //1
+// for (let a = 1; a <= 100; a++) {
+//         console.log(a);
+//     }
+
+// //2
+// for (let b = 1; b <= 100; b++) {
+//     if (b % 2 == 1) {
+//         console.log(b, "odd");
+//     }
+// }
+
+// //3
+// for (let c = 1; c <= 100; c++) {
+//     if (c % 2 == 0) {
+//         console.log(c, "even");
+//     }
+// }
+
+// //4
+// for (let d = 1; d <= 100; d++) {
+//     if (d % 5 == 0) {
+//         console.log(d);
+//     }
+// }
+
+// //5
+// for (let e = 1; e <= 100; e++) {
+//     if (e % 5 == 0 && e % 15 == 0) {
+//         console.log(e);
+//     }
+// }
+
+// //6
+// for (let f = 1; f <= 20; f++) {
+//     if (f % 2 == 0) {
+//         console.log(f);
+//     }
+// }
+
+// //7
+// for (let g = 1; g <= 5; g++) {
+//     if (g % 1 == 0) {
+//         console.log(g, "hello");
+//     }
+// }
+
+// //8
+// let sum = 0;
+// for (let h = 0; h <= 100; h++) {
+//     sum = sum + h
+// }
+// console.log(sum)
+
+// //9
+// let sums = 0
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 1) {
+//         sums = sums + i
+//     }
+// }
+// console.log(sums, "odd");
+
+// //10
+// let som = 0
+// for (let j = 1; j <= 100; j++) {
+//     if (j % 2 == 0) {
+//         som = som + j
+//     }
+// }
+// console.log(som, "even");
+
+// //11
+// for (let k = 1; k <= 2048; k *= 2) {
+//     if (k % 2 == 0) {
+//         console.log(k);
+//     }
+// }
+
+// // //12
+// for (let s = 1; s <= 100; s = s + 10) {
+//     console.log(s);
+//     for (let t = s; t < s + 10; t++) {
+//         document.write(t)
+//     }
+//     document.write("<br>")
+// }
+
+// //13
+
+// for (let l = 0; l <= 100; l++) {
+//     if (l % 3 == 1 && l % 5 == 1) {
+//         console.log("fizzbuzz");
+//     } else {
+//         console.log(l);
+//     }
+// }
+
+// //14
+// for (let r = 1; r <= 10; r++) {
+//     console.log(2 + "*" + r + "=" + r * 2);
+// }
+
+// 15
+// for (let y = 1; y <= 50; y = y + 10) {
+//     for (let z = y; z < y + 10; z++) {
+//         document.write(z + ",")
+//     }
+//     document.write("<br>")
+// }
+
+// //16
+// for (let ab = 50; ab >= 1; ab = ab - 10) {
+//     for (let bc = ab; bc > ab - 10; bc--) {
+//         document.write(bc + ",")
+//     }
+//     document.write("<br>")
+// }
+
+// //17
+// for (let o = 1; o <= 10000; o = o * 5) {
+//     console.log(o);
+// }
+
+// //18
+// for (let n = 1; n <= 500; n++) {
+//     if (n % 3 == 0) {
+//         console.log(n);
+//     }
+// }
+
+// //19
+// for (let m = 120; m <= 1029; m++) {
+//     if (m % 2 == 0) {
+//         console.log(m);
+//     }
+// }
+
+// //20
+// for (let p = 100; p >= 0; p--) {
+//     if (p % 5 === 0) {
+//         console.log(p);
+//     }
+// }
+
+// //21
+// let b = 70
+// for (let a = 1; a <= 70; a++) {
+//     if (b % a == 0) {
+//         console.log(a);
+//     }
+// }
+
+// //while loop
+// //1
+// let q = 1
+// while (q <= 10) {
+//     console.log(q);
+//     q++
+// }
+
+// 2
+// let u = 1
+// while (u <= 10) {
+//     if (u % 2 == 0) {
+//         console.log(u);
+//     }
+//     u++
+// }
+
+// //3
+// let v = 10
+// while (v >= 1) {
+//     console.log(v);
+//     v--
+// }
+
+// //4
+// let w = 1
+// while (w <= 20) {
+//     if (w % 2 == 0) {
+//         console.log(w);
+//     }
+//     w++
+// }
+
+// //5
+// let sums = 0
+// let x = 1
+// while (x <= 100) {
+//     if (x % 2 === 1) {
+//         sums = sums + x
+//     }
+//     x++
+// }
+// console.log(sums, "odd");
+
+// do while loop
+// //1
+// let cd = 1
+// do {
+//     console.log(cd);
+//     cd++
+// } while (cd <= 5);
+
+// //2
+// let de = 10
+// do {
+//     console.log(de);
+//     de--
+// } while (de >= 1);
+
+// //3
+// let sum = 0
+// let ef = 1
+// do {
+//     sum = sum + ef
+//     ef++
+// } while (ef <= 10);
+// console.log(sum);
+
+// //4
+// let fg = 1
+// do {
+//     if (fg % 2 == 0) {
+//         console.log(fg);
+//     }
+//     fg++
+// } while (fg <= 20);
+
+// //5
+// let ans = prompt("how many time do you want to print hello world")
+// let a = 1
+// do {
+//     console.log("hello");
+//     a++
+// } while (a <= ans);
+
+// //6
+// let sum = 0
+// let b = 1
+// do {
+//     if (b % 2 == 0) {
+//         sum = sum + b
+//     }
+//     b++
+// } while (b <= 100);
+// console.log(sum + "even");
+
+// //1
+// for (let a = 5; a >= 1; a--) {
+//     let str = ""
+//     for (let b = 1; b <= a; b++) {
+//         str = str + "*"
+//     }
+//     console.log(str);
+// }
+
+// //2
+// for (let a = 1; a <= 5; a++) {
+//     let = str = ""
+//     for (let b = 1; b <= a; b++) {
+//         str = str + "*"
+//     }
+//     console.log(str);
+// }
+
+// //3
+// for(let a=1;a<=5;a++){
+//     let =str=""
+//     for(let b=1;b<=a;b++){
+//         str=str+b
+//     }
+//     console.log(str);
+// }
+
+// //4
+// for(let c=5;c>=1;c--){
+//     let str=""
+//     for(let d=1;d<=c;d++){
+//         str =str+d
+//     }
+//     console.log(str);
+// }
+
+// //5
+// for (let a = 1; a <= 5; a++) {
+//     for (let b = 1; b <= 5; b++) {
+//         document.write("$");
+//     }
+//     document.write("<br>");
+// }
+
+// //6
+// for (let a = 1; a <= 10; a++) {
+//     let str = ""
+//     for (let b = 10; b >= a; b--) {
+//         str = str + b
+//     }
+//     console.log(str);
 // }
